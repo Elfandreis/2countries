9b77fc4170bef72808ac3e1c80c9bed088b09141
Elfandreis <elfandreis@gmail.com>
2019-08-09 22:08:07 +0300

    Yes

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-function maketea(origin, taste, effect)
-{
-	this.origin = origin;
-	this.taste = taste;
-	this.effect = effect;
+class tea {
+	constructor(origin, taste, effect) {
+		this.origin = origin;
+		this.taste = taste;
+		this.effect = effect;
+	}
 }
 
-let rotea= a;
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+var rotea = new tea(1, 2, 3);
+ctx.fillRect(10, 10, 10, 10);
